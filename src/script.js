6ad8ca0a454f6553e39ccdd66b58629850c2e38c
GabrielB4M5 <gabrielbatistutaams@gmail.com>
2024-05-27 17:37:19 -0300

    Cards audio content

--- src/script.js
+++ src/script.js
@@ -1,11 +1,21 @@
-function openCard(templateName) {
+function openCard(audioElementId, newSource, templateName) {
     const card = document.getElementById('card');
     const cardContent = document.getElementById('card-content');
     cardContent.textContent = templateName;
     card.style.display = 'block';
+    let audioElement = document.getElementById(audioElementId);
+    if (audioElement) {
+        let sourceElement = audioElement.getElementsByTagName('source')[0];
+        if (sourceElement) {
+            sourceElement.src = newSource;
+            audioElement.load();
+        }
+    }
 }
 
-function closeCard() {
+function closeCard(audioElementId) {
+    let audioElement = document.getElementById(audioElementId);
+    audioElement.pause();
     const card = document.getElementById('card');
     card.style.display = 'none';
 }
